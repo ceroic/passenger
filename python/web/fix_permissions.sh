28791f563a79d4c13a071fa1f4ffc6453a9f3aa7
Grant Means <grant@ceroic.com>
2015-05-27 16:45:00 -0400

    Fixed permissions script

--- python/web/fix_permissions.sh
+++ python/web/fix_permissions.sh
@@ -2,7 +2,7 @@
 if [ -d "/home/app/server/log/" ]
 then
   echo "Setting permissions on log directory."
-  chmod -R /home/app/server/log
+  chmod -R 775 /home/app/server/log
 else
   echo "No log directory found, skipping."
 fi
